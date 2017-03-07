112e9254629421dcb01c0d36814a2a4dd1f3fd05
Saeko Fukui <saeko0032@gmail.com>
2017-03-07 10:54:44 -0800

    Update ShoppingCart.m

--- ShoppingCart/ShoppingCart/ShoppingCart.m
+++ ShoppingCart/ShoppingCart/ShoppingCart.m
@@ -22,30 +22,12 @@
     [self.productItemArray addObject:product];
 }
 
-- (void)addProductPrice:(int)productID amount:(int)amount {
-    int price = 0;
-    if (productID == 412) {
-        price = 2 * amount;
-    } else if(productID == 183) {
-        price = 3 * amount;
-    } else if(productID == 100) {
-        price = 8 * 41 * amount;
-    } else if(productID == 101) {
-        price = 18 * 31 * amount;
-    } else if(productID == 701) {
-        price = 15 * amount;
-    }
-    NSLog(@"ID = %d price = %d", productID, price);
-    self.totalPrice += price;
-}
-
 - (int)totalProductsPrice {
     return self.totalPrice;
 }
 
 - (void)printAllPurchaseProducts {
-    for (int i = 0; i <self.productItemArray.count; i++) {
-    
+    for (int i = 0; i <self.productItemArray.count; i++) { 
         Products* p = [self.productItemArray objectAtIndex:i];
         NSLog(@"%@",p.productName);
     }
